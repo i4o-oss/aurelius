5a948655775e3aae115e9be369f2d7ebe954b028
Ilango <rajagopalilango@gmail.com>
2022-12-27 19:20:52 +0530

    📦 NEW: Initial commit

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
+import { ReactNode } from "react";
+export const Button = ({ children }: { children: ReactNode | string }) => {
+  return <button className='flex px-8 py-4 bg-red-900 text-white rounded-lg'>
+    {children}
+  </button>;
 };
