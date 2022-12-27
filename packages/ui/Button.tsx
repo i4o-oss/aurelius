a2c0683bf1b160777f423f7a08ef2ec24976bf28
Ilango <rajagopalilango@gmail.com>
2022-12-27 20:56:12 +0530

    🐛 FIX: Fix eslint and tsconfig issues

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,9 @@
-import * as React from "react";
-import { ReactNode } from "react";
+import * as React from 'react'
+import { ReactNode } from 'react'
 export const Button = ({ children }: { children: ReactNode | string }) => {
-  return <button className='flex px-8 py-4 bg-red-900 text-white rounded-lg'>
-    {children}
-  </button>;
-};
+	return (
+		<button className='flex px-8 py-4 bg-red-900 text-white rounded-lg'>
+			{children}
+		</button>
+	)
+}
